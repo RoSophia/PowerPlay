cb2be7d717164f52a8931cb0b04a1cd0f0262448
gitRaiku <sebi.pintilie@gmail.com>
2022-12-29 17:51:53 +0200

    Happy new years!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OP_Mode_mk1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OP_Mode_mk1.java
@@ -37,32 +37,31 @@ package org.firstinspires.ftc.teamcode;
  */
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
-
-import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaDriveToTargetWebcam;
 
 @Config
 @TeleOp
 public class OP_Mode_mk1 extends LinearOpMode {
     //sasiu
-    public DcMotor leftBack;
-    public DcMotor leftFront;
-    public DcMotor rightBack;
-    public DcMotor rightFront;
+    public DcMotorEx leftBack;
+    public DcMotorEx leftFront;
+    public DcMotorEx rightBack;
+    public DcMotorEx rightFront;
     //restu
-    public DcMotor ridicareSlide;
+    public DcMotorEx ridicareSlide;
     public Servo s1;
     public Servo s2;
-    public Servo sspate;
     public static double S1P = 0.24, S2P = 0.4; // Fucking you static
     //S2 0.4START 0.
     // 2 FINISH S1 0.24 START 0.5 FINISH
-    float s1pos, s2pos;
+    public static double s1pos;
     int poz = 0, stick;
     boolean lastButtonY = false;
     boolean lastButtonX = false;
@@ -73,10 +72,9 @@ public class OP_Mode_mk1 extends LinearOpMode {
     int lpos = 0;
 
     void ridicare(int target) {
-
         targetReading = target;
         ridicareSlide.setTargetPosition(targetReading);
-        ridicareSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        ridicareSlide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         ridicareSlide.setPower(1f);
         if (gamepad2.y) {
             s1pos = 0.0f;
@@ -101,55 +99,62 @@ public class OP_Mode_mk1 extends LinearOpMode {
     boolean switched = false;
 
     public static double AUT_POW = 0.811111212;
-    public static double POW_COEF = -0.7;
-    public static double COB_COEF = 0.3;
+    public static double POW_COEF = -0.9;
+    public static double COB_COEF = 0.5;
     public static boolean OV3RDR1V3 = false;
 
-    public static int dif = 130;
+    public static int BRAK = 1;
 
-    public int TOP_POS = 1353;
-    public int MIU_POS = 1060;
-    public int MID_POS = 810;
+    public static int dif = 150;
+
+    public static int TOP_POS = 1303;
+    public static int MIU_POS = 1010;
+    public static int MID_POS = 760;
+
+    public static double SDESCHIS = 0.75;
+    public static double SINCHIS = 0.25;
+    //PIDController controller;
+    //double p = 0, i = 0, d = 0;
+    //double pep = 0;
+    //double pap = 0;
 
     public void runOpMode() {
         VoltageSensor batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
 
+        //SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
         L2A = L2B = L2U = L2D = G2X = R2RB = R2LT = RB = switched = OV3RDR1V3 = false;
 
-        leftBack = hardwareMap.get(DcMotor.class, "LB");
-        leftFront = hardwareMap.get(DcMotor.class, "LF");
-        rightBack = hardwareMap.get(DcMotor.class, "RB");
-        rightFront = hardwareMap.get(DcMotor.class, "RF");
-        ridicareSlide = hardwareMap.get(DcMotor.class, "RS");
+        leftBack = hardwareMap.get(DcMotorEx.class, "LB");
+        leftFront = hardwareMap.get(DcMotorEx.class, "LF");
+        rightBack = hardwareMap.get(DcMotorEx.class, "RB");
+        rightFront = hardwareMap.get(DcMotorEx.class, "RF");
+        ridicareSlide = hardwareMap.get(DcMotorEx.class, "RS");
 
         s1 = hardwareMap.get(Servo.class, "S1");
         //s2 = hardwareMap.get(Servo.class, "S2");
-        sspate = hardwareMap.get(Servo.class, "RPS");
+        leftBack.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        leftFront.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
-        // leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        if (BRAK == 1) {
+            rightBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            leftBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        }
 
-        // rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        // leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        // rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        ridicareSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // ridicareSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        ridicareSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        ridicareSlide.setDirection(DcMotorSimple.Direction.REVERSE);
-        ridicareSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        ridicareSlide.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        // ridicareSlide.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        ridicareSlide.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        ridicareSlide.setDirection(DcMotorEx.Direction.REVERSE);
+        ridicareSlide.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         //reverse those suckers
-        rightBack.setDirection(DcMotor.Direction.REVERSE);
-        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.FORWARD);
+        leftFront.setDirection(DcMotorEx.Direction.FORWARD);
+        rightBack.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
 
         Servo SPa = hardwareMap.get(Servo.class, "SPa");
         Servo SPe = hardwareMap.get(Servo.class, "SPe");
@@ -157,8 +162,7 @@ public class OP_Mode_mk1 extends LinearOpMode {
         SPa.setPosition(0.0);
         SPe.setPosition(0.0);
 
-        s1pos = 0.85f;
-        //s2pos = 0.4f;
+        s1pos = 0.75f;
         ridicareSlide.setTargetPosition(0);
 
         waitForStart();
@@ -172,8 +176,6 @@ public class OP_Mode_mk1 extends LinearOpMode {
             final double rfPower = (speed * Math.cos(angle) - turn);
             final double lbPower = speed * Math.cos(angle) + turn;
             final double rbPower = (speed * Math.sin(angle) - turn);
-            //stick = (int) gamepad2.left_stick_y;
-            //poz=poz- stick;
 
             if (!L2A && gamepad2.a) {
                 ridicareSlide.setTargetPosition(TOP_POS);
@@ -209,32 +211,29 @@ public class OP_Mode_mk1 extends LinearOpMode {
             }
             R2LT = gamepad2.left_trigger > 0.01;
 
-            /*if (!RT && gamepad1.right_trigger > 0.01) {
-                slug = !slug;
-            }
-            RT = gamepad1.right_trigger > 0.01;*/
-
             if (USE_TELEMETRY) {
                 telemetry.clear();
                 telemetry.clearAll();
                 telemetry.addLine("PT");
+                //drive.updatePoseEstimate();
+                //telemetry.addData("Cpos", drive.getPoseEstimate());
             }
             if (OV3RDR1V3) {
                 if (USE_TELEMETRY) {
                     telemetry.addLine("ALL 0V3");
                 }
                 if (Math.abs(gamepad2.right_stick_y) > 0.01) {
-                    if (ridicareSlide.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
-                        ridicareSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                        ridicareSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    if (ridicareSlide.getMode() != DcMotorEx.RunMode.RUN_USING_ENCODER) {
+                        ridicareSlide.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+                        ridicareSlide.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
                     }
                     if (USE_TELEMETRY) {
                         telemetry.addLine("0V3RDR1V3 MAN");
                     }
                     ridicareSlide.setPower(POW_COEF * gamepad2.right_stick_y);
                 } else {
-                    if (ridicareSlide.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
-                        ridicareSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    if (ridicareSlide.getMode() != DcMotorEx.RunMode.RUN_TO_POSITION) {
+                        ridicareSlide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
                     }
                     if (USE_TELEMETRY) {
                         telemetry.addLine("0V3RDR1V3 AUT");
@@ -248,8 +247,8 @@ public class OP_Mode_mk1 extends LinearOpMode {
                     if (USE_TELEMETRY) {
                         telemetry.addLine("ALL MAN");
                     }
-                    if (ridicareSlide.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
-                        ridicareSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    if (ridicareSlide.getMode() != DcMotorEx.RunMode.RUN_USING_ENCODER) {
+                        ridicareSlide.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
                     }
                     lpos = ridicareSlide.getCurrentPosition();
                     if (lpos < 0) {
@@ -291,8 +290,8 @@ public class OP_Mode_mk1 extends LinearOpMode {
                         ridicareSlide.setTargetPosition(TOP_POS);
                     }
                 } else {
-                    if (ridicareSlide.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
-                        ridicareSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    if (ridicareSlide.getMode() != DcMotorEx.RunMode.RUN_TO_POSITION) {
+                        ridicareSlide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
                     }
                     if (USE_TELEMETRY) {
                         telemetry.addLine("ALL AUT");
@@ -335,49 +334,47 @@ public class OP_Mode_mk1 extends LinearOpMode {
                 telemetry.addData("Cpow", ridicareSlide.getPower());
                 telemetry.addData("left_stick_y", poz);
                 telemetry.addData("r_stick_y", gamepad2.right_stick_y);
+                telemetry.addData("pad", gamepad1.right_trigger);
                 telemetry.update();
             } else if (OV3RDR1V3) {
                 telemetry.addLine("U HAV3 3NT3R3D 0V3RDR1V3 M0D3");
                 telemetry.update();
             }
-            /*if(gamepad1.a)
-                s1.setPosition();
-            if(gamepad1.b) {   [[i              s1.setPosition();
-            }
-            if(gamepad1.x) {
-                s2.setPosition();
-            }
-            if (gamepad1.y) {
-                s2.setPosition();
-            }*/
-
-
-            //s1pos = (float) S1P;
-            //s2pos = (float) S2P;
-
 
             if (!G2X && gamepad2.x) {
                 if (switched) {
-                    s1pos = 0.85f;
+                    s1pos = SDESCHIS;
                 } else {
-                    s1pos = 0.25f;
+                    s1pos = SINCHIS;
                 }
                 switched = !switched;
             }
             G2X = gamepad2.x;
-
-
             s1.setPosition(s1pos);
-            //s2.setPosition(s2pos);
 
+
+
+            /*if (Math.abs(gamepad1.left_stick_x) +
+                Math.abs(gamepad1.left_stick_y) +
+                Math.abs(gamepad1.right_stick_x) +
+                Math.abs(gamepad1.right_stick_y) > 0.0001) {
+
+
+
+
+
+            } else {*/
             //dam blana in motoare
             double pcoef = 12.0 / batteryVoltageSensor.getVoltage();
             double spcoef = 1 - 0.6 * gamepad1.right_trigger;
             double fcoef = pcoef * spcoef;
-            leftFront.setPower (lfPower * fcoef);
+             leftFront.setPower(lfPower * fcoef);
             rightFront.setPower(rfPower * fcoef);
-            leftBack.setPower  (lbPower * fcoef);
-            rightBack.setPower (rbPower * fcoef);
+              leftBack.setPower(lbPower * fcoef);
+             rightBack.setPower(rbPower * fcoef);
+            //pep = SPe.getPosition();
+            //pap = SPa.getPosition();
+            //}
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -65,8 +65,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 90;// True max: 162.72
-    public static double MAX_ACCEL = 40;
+    public static double MAX_VEL = 100;// True max: 162.72
+    public static double MAX_ACCEL = 57;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
